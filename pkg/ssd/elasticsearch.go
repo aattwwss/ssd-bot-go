4ebb142e84d37bb5bc30d82616a7f2fd3a79e11d
alvin.tan <alvintan95@gmail.com>
2023-05-15 18:51:20 +0800

    implement find by id

--- pkg/ssd/elasticsearch.go
+++ pkg/ssd/elasticsearch.go
@@ -1,37 +1,94 @@
 package ssd
 
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+
+	"github.com/elastic/go-elasticsearch/v7"
+	"github.com/rs/zerolog/log"
+)
+
 type EsSSDRepository struct {
-	Host         string
-	Port         string
-	AccessKey    string
-	AccessSecret string
+	EsClient *elasticsearch.Client
+	Index    string
 }
 
-func NewEsSSDRepository(host, port, accessKey, accessSecret string) *EsSSDRepository {
+func NewEsSSDRepository(esClient *elasticsearch.Client, index string) *EsSSDRepository {
 	return &EsSSDRepository{
-		Host:         host,
-		Port:         port,
-		AccessKey:    accessKey,
-		AccessSecret: accessSecret,
+		EsClient: esClient,
+		Index:    index,
 	}
 }
 
-func (tpu *EsSSDRepository) FindById(id string) (*SSD, error) {
-	//TODO implement this
-	return &SSD{}, nil
+func (esRepo *EsSSDRepository) FindById(ctx context.Context, driveId string) (*SSD, error) {
+	var ssd SSD
+	query := map[string]interface{}{
+		"query": map[string]interface{}{
+			"term": map[string]interface{}{
+				"driveId": driveId,
+			},
+		},
+	}
+	err := esRepo.doSearch(ctx, query, &ssd)
+	if err != nil {
+		return nil, err
+	}
+	return &ssd, nil
 }
 
-func (tpu *EsSSDRepository) Search(s string) ([]BasicSSD, error) {
+func (esRepo *EsSSDRepository) Search(context context.Context, s string) ([]BasicSSD, error) {
 	//TODO implement this
 	return nil, nil
 }
 
-func (tpu *EsSSDRepository) Insert(ssd SSD) error {
+func (esRepo *EsSSDRepository) Insert(context context.Context, ssd SSD) error {
 	//TODO implement this
 	return nil
 }
 
-func (tpu *EsSSDRepository) Update(ssd SSD) error {
+func (esRepo *EsSSDRepository) Update(context context.Context, ssd SSD) error {
 	//TODO implement this
+	return nil
+}
+
+func (esRepo *EsSSDRepository) doSearch(ctx context.Context, query map[string]interface{}, payload any) error {
+	var buf bytes.Buffer
+	if err := json.NewEncoder(&buf).Encode(query); err != nil {
+		log.Error().Msgf("Error encoding query: %s", err)
+		return errors.New("error decoding query")
+	}
+	es := esRepo.EsClient
+	res, err := es.Search(
+		es.Search.WithContext(ctx),
+		es.Search.WithIndex(esRepo.Index),
+		es.Search.WithBody(&buf),
+	)
+	if err != nil {
+		log.Error().Msgf("Error getting response: %s", err)
+		return errors.New("search response error")
+	}
+	defer res.Body.Close()
+
+	if res.IsError() {
+		var e map[string]interface{}
+		if err := json.NewDecoder(res.Body).Decode(&e); err != nil {
+			log.Error().Msgf("Error parsing the response body: %s", err)
+		} else {
+			// Print the response status and error information.
+			log.Error().Msgf("[%s] %s: %s",
+				res.Status(),
+				e["error"].(map[string]interface{})["type"],
+				e["error"].(map[string]interface{})["reason"],
+			)
+		}
+		return errors.New("find by id response error")
+	}
+
+	if err := json.NewDecoder(res.Body).Decode(&payload); err != nil {
+		log.Error().Msgf("Error parsing the response body: %s", err)
+		return errors.New("search payload decode error")
+	}
 	return nil
 }
